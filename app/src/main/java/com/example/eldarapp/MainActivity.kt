756a78db79b5c97a708429dd23bff0d2c8de96a5
Fabian <fabian_carrazan@hotmail.com>
2023-10-19 12:51:49 -0300

    initial commit

---
+++ app/src/main/java/com/example/eldarapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.eldarapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.eldarapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
